9b721b2e3ce4a545148e85de07660461a9d44f53
philipaps <philipalvesline@gmail.com>
2021-06-27 22:45:28 -0300

    edit something

--- DjangoAdminProject_16_Fonte/DjangoAdminProject_16_Fonte/DjangoAdminProject/djangoAdmin/app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from .views import *
-
-
-urlpatterns = [
-    url(r'^veiculo/listar/(?P<categoria>[\w\-]+)/$', listar_veiculo, name='listar_veiculo'),
-    url(r'^veiculo/perfil/(?P<pk>[0-9]+)', perfil_veiculo, name='perfil_veiculo'),
-]
